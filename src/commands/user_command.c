436a0736cb7046e3b1e141addcdc8b7448522c93
Mahe-Evan <evan.mahe@epitech.eu>
2025-03-01 13:59:28 +0100

    fix 🐛 incorrect parsing in user

--- src/commands/user_command.c
+++ src/commands/user_command.c
@@ -10,6 +10,24 @@
 #include "../../includes/functs.h"
 #include <string.h>
 #include <unistd.h>
+#include <stdio.h>
+
+static int is_good(char *str)
+{
+    int i = 1;
+
+    if (str == NULL || str[0] != ' ') {
+        return 1;
+    }
+    while ((str[i] < 'a' || str[i] > 'z') &&
+        (str[i] < 'A' || str[i] > 'Z')) {
+        if (str[i] == '\t') {
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+}
 
 void user_command(server_t *server, client_t *client)
 {
@@ -17,12 +35,12 @@ void user_command(server_t *server, client_t *client)
         write(client->client_fd, "530 Already logged in\r\n", 24);
         return;
     }
-    if (strlen(client->command) < 6) {
+    if (is_good(client->command + 4) == 1) {
         write(client->client_fd,
-            "501 Syntax error in parameters or arguments\r\n", 45);
+            "530 Please login with USER and PASS\r\n", 37);
         return;
     }
-    if (strncmp(client->command + 5, "Anonymous", 9) == 0) {
+    if (strcasecmp(client->command + 5, "Anonymous") == 0) {
         strcpy(client->name, "Anonymous");
     }
     write(client->client_fd, "331 Please specify the password\r\n", 33);
