fafe39b66ed3b8c7e6fe4060ef4100e58f366eb0
Mahe-Evan <evan.mahe@epitech.eu>
2025-03-06 09:56:35 +0100

    fix 🐛 close client and loop inf

--- src/commands/stor_command.c
+++ src/commands/stor_command.c
@@ -10,7 +10,10 @@
 #include "../../includes/functs.h"
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 static void do_stor_command(client_t *client, char *path)
 {
@@ -18,18 +21,17 @@ static void do_stor_command(client_t *client, char *path)
     ssize_t bytes_read = 0;
     char buffer[2048] = {'\0'};
 
-    file_fd = open(path
-        , O_CREAT | O_WRONLY | O_TRUNC, 0666);
+    file_fd = open(path, O_CREAT | O_WRONLY | O_TRUNC, 0666);
     if (file_fd < 0) {
         write(client->client_fd, "550 Failed to open file.\r\n", 26);
         return;
     }
-    write(client->client_fd,
-        "150 Opening BINARY mode data connection.\r\n", 42);
-    bytes_read = read(client->data_fd, buffer, 2048);
+    write(client->client_fd, "150 Opening BINARY mode data connection.\r\n",
+        42);
+    bytes_read = read(file_fd, buffer, 2048);
     while (bytes_read > 0) {
         write(file_fd, buffer, bytes_read);
-        bytes_read = read(client->data_fd, buffer, 2048);
+        bytes_read = read(file_fd, buffer, 2048);
     }
     close(file_fd);
     close(client->data_fd);
@@ -38,15 +40,23 @@ static void do_stor_command(client_t *client, char *path)
 
 void stor_command(server_t *server, client_t *client)
 {
+    pid_t pid;
+
     server = server;
-    if (is_auth(client) == 1 || !check_data_connection(client)) {
-        return;
-    }
-    if (client->command[4] != ' ' && strlen(client->command) > 6) {
-        write(client->client_fd, "500 Unknown command\r\n", 21);
+    if (check_errors_clients(client) == 1) {
         return;
     }
     write(client->client_fd, "150 Ok to send data.\r\n", 22);
-    do_stor_command(client, client->command + 5);
-    write(client->client_fd, "226 Transfer complete.\r\n", 24);
+    pid = fork();
+    if (pid < 0) {
+        write(client->client_fd, "550 Failed to fork.\r\n", 21);
+        return;
+    } else if (pid == 0) {
+        do_stor_command(client, client->command + 5);
+        write(client->client_fd, "226 Transfer complete.\r\n", 24);
+        exit(0);
+    } else {
+        waitpid(pid, NULL, 0);
+        close_data_connection(client);
+    }
 }
